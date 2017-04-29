55fe8fc3ee617fe3e29874471142c92af67e79bd
Anubhav Tiwari <atworkstudios@gmail.com>
2017-04-29 12:40:26 +0530

    Delete export_Jb.js

--- brain/export_Jb.js
+++
@@ -1,8 +0,0 @@
-((lib)=>{
-    if(typeof module == undefined && typeof module.exports == undefined){
-        window.Jbrain = lib;
-    }
-    else{
-        module.exports = lib;
-    }
-})(Jbrain);
