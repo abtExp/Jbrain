71f76999716a8f337ccc4179ee274588ce4ec25b
Anubhav Tiwari <atworkstudios@gmail.com>
2017-05-07 23:15:33 +0530

    Update Jbrain.js

--- JBrain/Jbrain.js
+++ JBrain/Jbrain.js
@@ -3,7 +3,7 @@
 // import naive_bayes from 'nb';
 // import conv_net from 'convnet';
 
-Network = require('./brain/Network_re');
+Network = require('./brain/Network');
 // svm = require('./brain/svm');
 // naive_bayes = require('./brain/nb');
 // conv_net = require('./brain/convnet');
