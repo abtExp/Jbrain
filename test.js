5abaac9567786b164cb8f387305dfb9d9b16de49
Anubhav Tiwari <atworkstudios@gmail.com>
2017-05-27 13:41:29 +0530

    ..

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ var Jbrain = require('./Jbrain');
 
 var net = new Jbrain.Network([3,2,1]);
 
-var train_features = [[1,2,3],[4,2,0]];
-var train_labels = [0,1];
+var train_features = [[1,2,3],[4,2,0],[0,0,0]];
+var train_labels = [0,1,0];
 
 net.fit({train_features,train_labels, epoch : 4});
 
