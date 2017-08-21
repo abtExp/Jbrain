d7b2a015559e7d44e7001d52371da13f1a2936ce
abtExp <atworkstudios@gmail.com>
2017-08-21 11:38:55 +0530

    dash

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
+const dash = require('webpack-dashboard/plugin');
 module.exports = {
-    entry : path.resolve(__dirname,'Jbrain.js'),
-    output :{
-        path : path.resolve(__dirname,'build'),
-        filename : 'build.js'
+    entry: path.resolve(__dirname, 'Jbrain.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'build.js'
     },
     module: {
-        loaders: [{ 
-            test: /\.js$/, 
-            exclude: [/node_modules/,/build/], 
-            loader: "babel-loader" 
+        loaders: [{
+            test: /\.js$/,
+            exclude: [/node_modules/, /build/],
+            loader: "babel-loader"
         }]
-    }
+    },
+    plugins: [new dash()]
 };
