378263e46939a07a471272bdcc3ef5abfbe8734a
abtExp <atworkstudios@gmail.com>
2017-09-09 18:53:49 +0530

    added jest for unit testing

---
+++ jbrain.test.js
@@ -0,0 +1,25 @@
+/* Unified Test File For All Unit Tests For Jbrain 
+ * Any Unit Test For Further Development Will Be Added To 
+ * The End Of This File.
+ * 
+ * Author : AbT <atworkstudios@gmail.com>
+ * Project : Jbrain
+ * CodeName : JSimpl
+ *  
+ */
+
+/* Importing The Library */
+
+const { Network } = require('./Jbrain');
+
+
+/* Tests For Creation Of The Network */
+
+
+/* Method 1 : Simple FeedForward Network Simple Default Creation */
+test('Creating A Network Method 1', () => {
+    let net = new Network([25, 10, 5, 2]);
+    expect().toBe();
+})
+
+/* Method 2 : Simple FeedForward Network Simple Customized Creation */
