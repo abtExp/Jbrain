77a446af0878f80e9c9deebee13b0029a3bd74db
abtExp <atworkstudios@gmail.com>
2017-11-07 20:06:14 +0530

    housekeeping

--- util/costs/quadCost.js
+++ util/costs/quadCost.js
@@ -1,14 +1,11 @@
 const { math, core } = require('vecto');
 
 function quadCost(a, y, m) {
-    console.log(core.calc_shape(a));
     let cost = math.diff(a, y);
     cost = math.pow(cost, 2);
     cost = math.sum(cost, null, 1);
     cost = math.product((1 / (2 * m)), cost);
-    console.log(cost);
     cost = math.sum(cost);
-    console.log(cost);
 }
 
 quadCost.grad = (a, y, m) => {
